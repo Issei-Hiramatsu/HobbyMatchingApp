bb08af5ac0dec9c99ffc3a6ac5d39ae598eb6489
issei <issei.hiramatsu@gmail.com>
2022-12-06 11:52:40 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Hobby Matching',
+      home: null,
+    );
+  }
+}
